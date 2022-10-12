422b1c3cea5a4da55cdf4ee28b3c881b3e294920
Sakhile <Sakhile@codeplex.co.za>
2022-10-12 05:55:15 +0200

    Add project files.

---
+++ LinearSearchAlgo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinearSearchAlgo
+{
+    class Program
+    {
+        static int search(int[] arr, int n, int x)
+        {
+            for  (int i = 0; i < n; i++)
+            {
+                if (arr[i] == x)
+                    return i;
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 10, 30, 15 };
+            int x = 30;
+            int n = arr.Length;
+
+            if (search(arr, n, x) > 0)
+                Console.WriteLine(x + " is present at index " + search(arr, n, x));
+            else
+                Console.WriteLine(x + " is not present in the array.");
+        }
+    }
+}
